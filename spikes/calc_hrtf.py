bc94a2582018c6ef3200c756d138fd2c0140aa74
justanotherpaul <justanotherpaul@ac02799c-d943-0410-ae75-af7dd05a0959>
2008-01-21 18:36:08 +0000

    calc_hrtf changed to an object oriented model, cleaned up as well.  get_angle and then the make_filter function still need to be completed (plus everything else :).

--- spikes/calc_hrtf.py
+++ spikes/calc_hrtf.py
@@ -1,3 +1,74 @@
+import wave
+
+# Configuration variables.  You might need to change these.
+global_config = {
+            'HRIR_DIR' : 'h:\laforge\hrirs\\',
+            'NUM_TAPS' : 128,
+            'ANGLE_STEP' : 5,
+            }
+
+class SoundSourceFilter():
+    def __init__(self):
+            # State variables
+            self._angle = 0.0
+            
+            # Initialize HRIR tables
+            self._hrirs = {}
+            for angle in range(0, 360, global_config['ANGLE_STEP']):
+                self._hrirs[angle] = self.read_hrir_from_wav(
+                        global_config['HRIR_DIR'] + 'L0e%.3da.wav' % angle)
+
+            # Initialize initial filter references
+            self._filter_refs = [0]*global_config['NUM_TAPS']
+            
+
+    def tick(self):
+        '''
+        Modify the filter references to represent the next tick.
+        '''
+
+        # TODO: this will most likely be implemented on the hardware as a
+        # circular buffer.
+        
+        # Remove the last filter reference
+        self._filter_refs.pop(len(self._filter_refs)-1)
+
+        # Insert a new filter reference at the front
+        self._filter_refs.insert(0, self.get_angle())
+
+    def get_angle(self):
+        '''
+        Returns a quantized version of the angle.
+        '''
+
+        # TODO: this is where you left off
+
+
+
+
+    @classmethod
+    def read_hrir_from_wav(self, filename):
+        '''
+        Processes only the first channel of the file.
+        '''
+        w = wave.open(filename, "rb")
+        
+        (nchannels, sampwidth, framerate, 
+            nframes, comptype, compname) = w.getparams()
+        
+        r = []
+    
+        data = w.readframes(nframes)
+    
+        for i in range(0, nframes*sampwidth, sampwidth):
+            v = 0
+            for j in range(0, sampwidth):
+               v += ord(data[i+j]) << 8*(sampwidth-j-1)
+            r.append(v)
+    
+        return r
+
+
 def make_delay(d, taps):
     F = [0.0]*taps
     F[d] = 1.0
