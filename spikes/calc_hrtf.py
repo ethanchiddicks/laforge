4a1342ba45a13b6fefed35f8a8cae98d39335bc6
justanotherpaul <justanotherpaul@ac02799c-d943-0410-ae75-af7dd05a0959>
2008-01-30 21:41:32 +0000

    calc_hrtf now does time-varying direction filtering.  Distance still needs to be added.  Noted issues are FIR gain, equalization, interpolation between direction filters, and lack of reverberation causing externalization perception to be missing.

--- spikes/calc_hrtf.py
+++ spikes/calc_hrtf.py
@@ -1,50 +1,96 @@
 import wave
+import ctypes
 
 # Configuration variables.  You might need to change these.
 global_config = {
-            'HRIR_DIR' : 'h:\laforge\hrirs\\',
+            'HRIR_DIR' : '/Users/paulwalker/laforge/hrirs/',
             'NUM_TAPS' : 128,
             'ANGLE_STEP' : 5,
             }
 
 class SoundSourceFilter():
-    def __init__(self):
-            # State variables
-            self._angle = 0.0
-            
+    def __init__(self, channel):
             # Initialize HRIR tables
             self._hrirs = {}
             for angle in range(0, 360, global_config['ANGLE_STEP']):
                 self._hrirs[angle] = self.read_hrir_from_wav(
-                        global_config['HRIR_DIR'] + 'L0e%.3da.wav' % angle)
+                        global_config['HRIR_DIR'] + '%s0e%.3da.wav' % (channel, angle))
 
             # Initialize initial filter references
             self._filter_refs = [0]*global_config['NUM_TAPS']
-            
 
-    def tick(self):
+            # Initialize the initial signal buffer
+            self._signal_buff = [0]*global_config['NUM_TAPS']
+
+    def process_signal(self, signal, angle):
+        r = []
+        for i in range(0, len(signal)):
+            a = float(i)/len(signal)*180.0+angle
+            o = self.tick(signal[i], a)
+            r.append(o)
+        return r
+
+    def tick(self, next_sample, angle):
+        self._ref_tick(angle)
+
+        self._buff_tick(next_sample)
+
+        return self._calc_output()
+
+    def _calc_output(self):
+
+        f = self._calc_filter()
+
+        # Multiply accumulate
+        a = 0
+        for i in range(0, global_config['NUM_TAPS']):
+            s = f[i] * self._signal_buff[i]
+	    a += s
+
+        return a
+            
+    def _calc_filter(self):
+        f = []
+        for i in range(0, global_config['NUM_TAPS']):
+            f.append(self._hrirs[self._filter_refs[i]][i])
+        return f
+
+    def _buff_tick(self, next_sample):
+        '''
+        Modify the signal buffer to represent the next tick.
+        '''
+
+        # Note: this will most likely be implemented on the hardware as a
+        # circular buffer.
+        
+        # Remove the last sample
+        self._signal_buff.pop(len(self._signal_buff)-1)
+
+        # Insert a new sample at the front
+        self._signal_buff.insert(0, next_sample)
+
+    def _ref_tick(self, angle):
         '''
         Modify the filter references to represent the next tick.
         '''
 
-        # TODO: this will most likely be implemented on the hardware as a
+        # Note: this will most likely be implemented on the hardware as a
         # circular buffer.
         
         # Remove the last filter reference
         self._filter_refs.pop(len(self._filter_refs)-1)
 
         # Insert a new filter reference at the front
-        self._filter_refs.insert(0, self.get_angle())
+        self._filter_refs.insert(0, self.quantize_angle(angle))
 
-    def get_angle(self):
+    def quantize_angle(self, angle):
         '''
-        Returns a quantized version of the angle.
+        Returns a floor-quantized version of the angle in the range of 0-360.
         '''
-
-        # TODO: this is where you left off
-
-
-
+        
+        angle = angle % 360
+        r = int(int(angle / global_config['ANGLE_STEP']) * global_config['ANGLE_STEP'])
+        return r
 
     @classmethod
     def read_hrir_from_wav(self, filename):
@@ -55,6 +101,9 @@ class SoundSourceFilter():
         
         (nchannels, sampwidth, framerate, 
             nframes, comptype, compname) = w.getparams()
+
+        if sampwidth != 2:
+            raise Exception("Can only handle 16 bit wav files.")
         
         r = []
     
@@ -62,45 +111,47 @@ class SoundSourceFilter():
     
         for i in range(0, nframes*sampwidth, sampwidth):
             v = 0
-            for j in range(0, sampwidth):
-               v += ord(data[i+j]) << 8*(sampwidth-j-1)
+            v += ord(data[i]) 
+            v += ord(data[i+1]) << 8
+	    v = ctypes.c_short(v).value
             r.append(v)
     
         return r
 
+    @classmethod
+    def write_to_wav(self, filename, o, gain):
+        # Normalize signal to -(2**15) to (2**15)-1 range. 
+        peak = max(max(o), min(o)*-1)
+        o = [int(float(sample)*gain*(2**15-1)) for sample in o]
+    
+        w = wave.open(filename, "wb")
 
-def make_delay(d, taps):
-    F = [0.0]*taps
-    F[d] = 1.0
-    return F
+        w.setnchannels(1)
+        w.setsampwidth(2)
+        w.setframerate(44100)
 
-def make_ramp(l, p):
-    r = [None]*l
-    for i in range(0, l):
-        r[i] = (i%p)/(float(p)-1.0)
-    return r
+        # Turn into string
+        o2 = ""
+        for s in o:
+            o2 += chr(s&0x00FF)
+            o2 += chr((s&0xFF00)>>8)
 
-def make_firs(num_fir, num_tap):
-    # Generate FIRs with all possible delays
-    f = [None]*num_fir
-    for i in range(0, num_fir):
-        f[i] = make_delay(i, num_tap)
-    return f
+        w.writeframes(o2)
 
 if __name__ == '__main__':
-    num_fir = 4
-    num_tap = 4
+    a = 270.0
 
-    f = make_firs(num_fir, num_tap)
-    s = make_ramp(10, 5)
-
-    fr = [0]*num_tap
-
-    fr[3] = 1
-
-    F = [None]*num_fir
-
-    for i in range(0, num_tap):
-        F[i] = f[fr[i]][i]
-
-    print F
+    s = SoundSourceFilter('R')
+    i = s.read_hrir_from_wav("/Users/paulwalker/laforge/sources/whitenoise.wav")
+    o_right = s.process_signal(i, a)
+    
+    s = SoundSourceFilter('L')
+    i = s.read_hrir_from_wav("/Users/paulwalker/laforge/sources/whitenoise.wav")
+    o_left = s.process_signal(i, a)
+    
+    peak_right = max(max(o_right), min(o_right)*-1)
+    peak_left = max(max(o_left), min(o_left)*-1)
+    peak = max(peak_left, peak_right)
+    gain = 1.0/peak
+    s.write_to_wav("blahL.wav", o_left, gain)
+    s.write_to_wav("blahR.wav", o_right, gain)
